d4941ad6ebb8f86c46a6956ced8c5a74dbab6d4a
iTop <itop@192-168-0-100.rdsnet.ro>
2018-12-18 12:59:54 +0200

    add random bg color and random circle color

--- canvasCircles.js
+++ canvasCircles.js
@@ -14,23 +14,19 @@ function random(number) {
 }
 
 function draw() {
+    document.body.style.backgroundColor = color(255,255,255,0.9);
     ctx.clearRect(0,0,WIDTH,HEIGHT);
     for(let i = 0; i < 100; i++) {
         ctx.beginPath();
-        ctx.fillStyle = 'rgba(255,0,0,0.5)';
+        ctx.fillStyle = color(255,100,100,0.5); //'rgba(255,0,0,0.5)';
         ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
         ctx.fill();
     }
 }
 
-function bgColor() {
-    let randomColor = 'rgb('+random(255)+','+random(255)+','+random(255)+')';
-    document.body.style.backgroundColor = randomColor;
+function color(red,green,blue,opacity) {
+    let randomColor = 'rgb('+random(red)+','+random(green)+','+random(blue)+','+opacity+')';
+    return randomColor;
 }
 
-function start() {
-    draw();
-    bgColor();
-}
-
-btn.addEventListener('click',start);
+btn.addEventListener('click',draw);
